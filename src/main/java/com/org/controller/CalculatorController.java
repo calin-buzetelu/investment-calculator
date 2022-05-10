d2554452ad2b86e58473b8a0994ea5c081145d96
Calin Buzetelu <calinbuze@gmail.com>
2022-05-10 10:00:55 +0200

    Add DataEntry object to the model

--- src/main/java/com/org/controller/CalculatorController.java
+++ src/main/java/com/org/controller/CalculatorController.java
@@ -1,13 +1,18 @@
 package com.org.controller;
 
+import com.org.entity.DataEntry;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CalculatorController {
 
 	@RequestMapping("/")
-	public String showWelcomePage(){
+	public String showWelcomePage(Model model){
+		DataEntry dataEntry = new DataEntry();
+		model.addAttribute("dataEntry", dataEntry);
 		return "welcomePage";
 	}
 }
