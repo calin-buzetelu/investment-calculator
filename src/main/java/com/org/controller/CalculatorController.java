02453d50439b537421b8cf7e8e24c6779d8ee39e
Calin Buzetelu <calinbuze@gmail.com>
2022-05-11 12:54:46 +0200

    Add controller side validation logic

--- src/main/java/com/org/controller/CalculatorController.java
+++ src/main/java/com/org/controller/CalculatorController.java
@@ -3,8 +3,11 @@ package com.org.controller;
 import com.org.entity.DataEntry;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class CalculatorController {
@@ -17,7 +20,10 @@ public class CalculatorController {
 	}
 
 	@RequestMapping("/showFutureValue")
-	public String showFutureValue(Model model, @ModelAttribute("dataEntry") DataEntry dataEntry){
+	public String showFutureValue(Model model, @Valid @ModelAttribute("dataEntry") DataEntry dataEntry, BindingResult bindingResult){
+		if(bindingResult.hasErrors()){
+			return "welcomePage";
+		}
 		double futureValue = dataEntry.computeFutureValue();
 		model.addAttribute("futureValue", futureValue);
 		return "futureValueConfirmation";
